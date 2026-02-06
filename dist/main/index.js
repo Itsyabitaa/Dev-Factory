1b7d317068e3c8df4e83dffe33e94fb78c261c6f
kukusha <yeabsira0514@gmail.com>
2026-02-06 16:03:54 +0300

    Refactor index.js to use named imports for Electron and improve module compatibility

--- dist/main/index.js
+++ dist/main/index.js
@@ -1,38 +1,39 @@
-import { app, BrowserWindow, ipcMain } from "electron";
-import path from "path";
-import { fileURLToPath } from "url";
-import { CommandRunner } from "./core/commandRunner.js";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+const path_1 = __importDefault(require("path"));
+const commandRunner_1 = require("./core/commandRunner");
 let mainWindow = null;
-const runner = new CommandRunner();
+const runner = new commandRunner_1.CommandRunner();
 function createWindow() {
-    mainWindow = new BrowserWindow({
+    mainWindow = new electron_1.BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, "../preload/preload.js"),
+            preload: path_1.default.join(__dirname, "../preload/preload.js"),
             contextIsolation: true,
             nodeIntegration: false,
-            // Disable sandbox so ES module preload can use `import` syntax
             sandbox: false,
         },
     });
-    mainWindow.loadFile(path.join(__dirname, "../../src/renderer/index.html"));
+    mainWindow.loadFile(path_1.default.join(__dirname, "../../src/renderer/index.html"));
 }
-app.whenReady().then(() => {
+electron_1.app.whenReady().then(() => {
     createWindow();
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0)
+    electron_1.app.on("activate", () => {
+        if (electron_1.BrowserWindow.getAllWindows().length === 0)
             createWindow();
     });
 });
-app.on("window-all-closed", () => {
+electron_1.app.on("window-all-closed", () => {
     if (process.platform !== "darwin")
-        app.quit();
+        electron_1.app.quit();
 });
 // IPC Handlers
-ipcMain.handle("cmd:run", async (_event, { jobId, command, args, options }) => {
+electron_1.ipcMain.handle("cmd:run", async (_event, { jobId, command, args, options }) => {
     try {
         const result = await runner.run(jobId, command, args, options, {
             onStdout: (data) => {
@@ -48,6 +49,6 @@ ipcMain.handle("cmd:run", async (_event, { jobId, command, args, options }) => {
         return { success: false, error: error.message };
     }
 });
-ipcMain.on("cmd:cancel", (_event, jobId) => {
+electron_1.ipcMain.on("cmd:cancel", (_event, jobId) => {
     runner.cancel(jobId);
 });
