63af6887454b4ddf6706d8c9d7ba497edd22aa3f
kukusha <yeabsira0514@gmail.com>
2026-02-06 16:04:07 +0300

    Refactor preload.js to use named imports for Electron and improve module structure

--- dist/preload/preload.js
+++ dist/preload/preload.js
@@ -1,6 +1,8 @@
-import { contextBridge, ipcRenderer } from "electron";
-contextBridge.exposeInMainWorld("electronAPI", {
-    runCommand: (jobId, command, args, options) => ipcRenderer.invoke("cmd:run", { jobId, command, args, options }),
-    cancelCommand: (jobId) => ipcRenderer.send("cmd:cancel", jobId),
-    onCommandLog: (callback) => ipcRenderer.on("cmd:log", (_event, value) => callback(value)),
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const electron_1 = require("electron");
+electron_1.contextBridge.exposeInMainWorld("electronAPI", {
+    runCommand: (jobId, command, args, options) => electron_1.ipcRenderer.invoke("cmd:run", { jobId, command, args, options }),
+    cancelCommand: (jobId) => electron_1.ipcRenderer.send("cmd:cancel", jobId),
+    onCommandLog: (callback) => electron_1.ipcRenderer.on("cmd:log", (_event, value) => callback(value)),
 });
